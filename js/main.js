37e0695ac7d48cc5d8f78cd6801d3c619d363e8f
SAKPand <123720317+SAKPand@users.noreply.github.com>
2023-06-10 17:55:12 +0530

    trvels

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+let menu = document.getElementById('menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
 
 
 var swiper = new Swiper(".home-slider",{
@@ -27,15 +39,3 @@ var swiper = new Swiper(".reviews-slider",{
     }
 })
 
-// let menu=document.querySelectorAll('#menu-btn');
-// let navbar= document.querySelectorAll('.header .navbar');
-
-// menu.onclick =() =>{
-//     menu.classList.toggle('fa-times');
-//     navbar.classList.toggle('active'); 
-// } 
-
-// window.onscroll =()=>{
-//     menu.classList.remove('fa-times');
-//     navbar.classList.remove('active')
-// }
